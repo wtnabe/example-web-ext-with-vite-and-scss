d2a35da989fd1cc62e50dc9830388783d2091ea9
wtnabe <18510+wtnabe@users.noreply.github.com>
2022-02-12 07:21:12 +0900

    enable sourcemap for development mode

--- vite.config.js
+++ vite.config.js
@@ -3,23 +3,26 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import webExtension from 'vite-plugin-web-extension'
 
-export default defineConfig({
-  root: 'src',
-  build: {
-    outDir: path.resolve(__dirname, 'dist'),
-    emptyOutDir: true
-  },
-  plugins: [
-    webExtension({
-      manifest: () => {
-        const packageJson = require('./package.json')
-        return {
-          ...require('./src/manifest.json'),
-          name: packageJson.name,
-          version: packageJson.version
-        }
-      },
-      assets: 'assets'
-    })
-  ]
+export default defineConfig(({ command, mode }) => {
+  return {
+    root: 'src',
+    build: {
+      outDir: path.resolve(__dirname, 'dist'),
+      emptyOutDir: true,
+      sourcemap: (mode === 'development') ? 'inline' : false
+    },
+    plugins: [
+      webExtension({
+        manifest: () => {
+          const packageJson = require('./package.json')
+          return {
+            ...require('./src/manifest.json'),
+            name: packageJson.name,
+            version: packageJson.version
+          }
+        },
+        assets: 'assets'
+      })
+    ]
+  }
 })
